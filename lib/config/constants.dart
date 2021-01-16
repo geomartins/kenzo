6485b83264aa05b37e33b56a79e755088cd3ae31
Martins Abiodun <martinsabiodun94@gmail.com>
2021-01-16 14:58:44 +0100

    preparing to deploy

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -6,4 +6,4 @@ const kPrimaryColor = Colors.teal;
 const kTertiaryColor = Colors.grey;
 const kDefaultProfileUrl = 'assets/images/ticket-logo.png';
 const kDefaultCoverUrl = 'assets/images/ticket-logo.png';
-const kDefaultFileUrl = 'assets/images/file.png';
+const kDefaultFileUrl = 'assets/images/ticket-logo.png';
